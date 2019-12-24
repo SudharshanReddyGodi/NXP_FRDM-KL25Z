4e998af2a202cafce98a7a5a1d81c33a527830eb
sudharshan406 <sudharshan406@gmail.com>
2019-12-24 22:27:42 +0530

    Reference manual for the Evolution board added in DOC folder

--- MKL25Z128VLK4/Debug/CMSIS/subdir.mk
+++ MKL25Z128VLK4/Debug/CMSIS/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 CMSIS/%.o: ../CMSIS/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -D__NEWLIB__ -DCPU_MKL25Z128VLK4 -DCPU_MKL25Z128VLK4_cm0plus -DSDK_OS_BAREMETAL -DFSL_RTOS_BM -DSDK_DEBUGCONSOLE=0 -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I"D:\MCUXpressoWorkSpace\MKL25Z128VLK4\board" -I"D:\MCUXpressoWorkSpace\MKL25Z128VLK4\source" -I"D:\MCUXpressoWorkSpace\MKL25Z128VLK4" -I"D:\MCUXpressoWorkSpace\MKL25Z128VLK4\drivers" -I"D:\MCUXpressoWorkSpace\MKL25Z128VLK4\CMSIS" -I"D:\MCUXpressoWorkSpace\MKL25Z128VLK4\utilities" -I"D:\MCUXpressoWorkSpace\MKL25Z128VLK4\startup" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fmerge-constants -fmacro-prefix-map="../$(@D)/"=. -mcpu=cortex-m0plus -mthumb -D__NEWLIB__ -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-gcc -D__NEWLIB__ -DCPU_MKL25Z128VLK4 -DCPU_MKL25Z128VLK4_cm0plus -DSDK_OS_BAREMETAL -DFSL_RTOS_BM -DSDK_DEBUGCONSOLE=0 -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I"D:\Git_repo\NXP_FRDM-KL25Z\MKL25Z128VLK4\board" -I"D:\Git_repo\NXP_FRDM-KL25Z\MKL25Z128VLK4\source" -I"D:\Git_repo\NXP_FRDM-KL25Z\MKL25Z128VLK4" -I"D:\Git_repo\NXP_FRDM-KL25Z\MKL25Z128VLK4\drivers" -I"D:\Git_repo\NXP_FRDM-KL25Z\MKL25Z128VLK4\CMSIS" -I"D:\Git_repo\NXP_FRDM-KL25Z\MKL25Z128VLK4\utilities" -I"D:\Git_repo\NXP_FRDM-KL25Z\MKL25Z128VLK4\startup" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fmerge-constants -fmacro-prefix-map="../$(@D)/"=. -mcpu=cortex-m0plus -mthumb -D__NEWLIB__ -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
